7627d96284c1b10036f8d465f230452418436aa1
Harsh <harsh160502@gmail.com>
2024-12-14 00:06:57 +0530

    added

--- src/Arrayss/LargestElementArray.java
+++ src/Arrayss/LargestElementArray.java
@@ -6,11 +6,11 @@ public class LargestElementArray {
     public static void main(String[] args) {
         int[] arr = new int[]{3, 2, 1, 5, 2};
         int largest = arr[0];
-        int[] var3 = arr;
-        int var4 = arr.length;
+        int[] a = arr;
+        int n = arr.length;
 
-        for(int var5 = 0; var5 < var4; ++var5) {
-            int val = var3[var5];
+        for(int i = 1; i < n; i++) {
+            int val = a[i];
             if (val > largest) {
                 largest = val;
             }

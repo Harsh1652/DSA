511cbd863597c6040fa63006504fcd6c89d0f59c
HarshExcollo <your-email@example.com>
2026-02-07 01:50:48 +0530

    update

---
+++ src/BinaryTrees/BoundaryTraversal.java
@@ -0,0 +1,88 @@
+package BinaryTrees;
+
+import java.util.ArrayList;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BoundaryTraversal {
+
+    boolean isLeaf(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+
+    void addLeftBoundary(TreeNode root, ArrayList<Integer> res) {
+        TreeNode cur = root.left;
+
+        while (cur != null) {
+            if (!isLeaf(cur)) {
+                res.add(cur.val);
+            }
+
+            if (cur.left != null) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+    }
+
+    void addLeaves(TreeNode root, ArrayList<Integer> res) {
+        if (root == null) return;
+
+        if (isLeaf(root)) {
+            res.add(root.val);
+            return;
+        }
+
+        addLeaves(root.left, res);
+        addLeaves(root.right, res);
+    }
+
+    void addRightBoundary(TreeNode root, ArrayList<Integer> res) {
+        TreeNode cur = root.right;
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        while (cur != null) {
+            if (!isLeaf(cur)) {
+                temp.add(cur.val);
+            }
+
+            if (cur.right != null) {
+                cur = cur.right;
+            } else {
+                cur = cur.left;
+            }
+        }
+
+        for (int i = temp.size() - 1; i >= 0; i--) {
+            res.add(temp.get(i));
+        }
+    }
+
+    ArrayList<Integer> boundaryTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        if (!isLeaf(root)) {
+            res.add(root.val);
+        }
+
+        addLeftBoundary(root, res);
+        addLeaves(root, res);
+        addRightBoundary(root, res);
+
+        return res;
+    }
+}
+
+
+// TC - O(N)
+// SC - O(N)
+
---
+++ src/BinaryTrees/ZigZagTraversal.java
@@ -0,0 +1,54 @@
+package BinaryTrees;
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class ZigZagTraversal {
+    
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (leftToRight) {
+                    currentLevel.add(node.val);
+                }
+                else {
+                    currentLevel.add(0, node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            result.add(currentLevel);
+            leftToRight = !leftToRight;
+        }
+        return result;
+    }
+}
+
+// TC - O(N)
+// SC - O(N)
